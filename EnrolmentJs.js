96d822e3856646470d7933a461897024867e261f
shoug aljebreen <shougaljebreen@gmail.com>
2023-11-22 20:12:09 +0300

    dropdown

--- EnrolmentJs.js
+++ EnrolmentJs.js
@@ -1,52 +1,21 @@
-
 //1
 
-// Function to filter children's names based on the selected option
-function filterChildren() {
-    var selectedKid = document.getElementById("kids-filter").value;
+document.addEventListener("DOMContentLoaded", function () {
+    // Retrieve child names from local storage
+    var childNames = JSON.parse(localStorage.getItem("childNames")) || [];
 
-    // Retrieve children's names from local storage
-    var storedNames = getStoredChildrenNames();
-
-    // Filter and display names based on the selected option
-    var filteredNames = (selectedKid === "all") ? storedNames : [selectedKid];
-    displayFilteredNames(filteredNames);
-}
-
-// Function to retrieve children's names from local storage
-function getStoredChildrenNames() {
-    // Retrieve the data from local storage
-    var storedInfo = JSON.parse(localStorage.getItem("childInfo"));
-    if (!storedInfo) {
-        return [];
-    }
-
-    // Extract and return an array of children's names
-    return storedInfo.map(function (child) {
-        return child.name;
-    });
-}
-
-// Function to display filtered names
-function displayFilteredNames(names) {
-    // Clear existing names in the dropdown
-    var dropdown = document.getElementById("kids-filter");
-    dropdown.innerHTML = '<option value="all">All Kids</option>';
-
-    // Populate the dropdown with filtered names
-    names.forEach(function (name) {
+    // Populate the kids filter dropdown with the retrieved child names
+    var kidsFilterDropdown = document.getElementById("kids-filter");
+    childNames.forEach(function (childName) {
         var option = document.createElement("option");
-        option.value = name;
-        option.text = name;
-        dropdown.add(option);
+        option.value = childName;
+        option.textContent = childName;
+        kidsFilterDropdown.appendChild(option);
     });
-}
 
-// Initial setup to populate the dropdown with names from local storage
-window.onload = function () {
-    var storedNames = getStoredChildrenNames();
-    displayFilteredNames(storedNames);
-};
+    // You can do further processing or UI updates with the retrieved child names here
+});
+
 
 //2,3,4
 
