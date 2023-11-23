313905114e01924a46e6c48a595f27aec81ebd34
shoug aljebreen <shougaljebreen@gmail.com>
2023-11-23 17:52:11 +0300

    changes

--- EnrolmentJs.js
+++ EnrolmentJs.js
@@ -21,140 +21,103 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-//2,3,4
-
-var courses = [
-    {
-        name: "Java",
-        tutor: "Fahad Omar",
-        prerequisite: "Introduction to Programming"
-    },
-    {
-        name: "Python",
-        tutor: "Sami Ahmed",
-        prerequisite: "Introduction to Programming"
-    },
-    {
-        name: "Scratch",
-        tutor: "Hind Saleh",
-        prerequisite: "Computer Basics"
-    },
-    {
-        name: "HTML",
-        tutor: "Amal Abdullah",
-        prerequisite: "Introduction to Web Development"
-    },
-    {
-        name: "Machine Learning",
-        tutor: "Najwa Hamed",
-        prerequisite: "Python"
-    },
-    {
-        name: "Robotics",
-        tutor: "Mona Khalid",
-        prerequisite: "Introduction to Programming"
-    },
-    {
-        name: "JavaScript",
-        tutor: "Yousef Ali",
-        prerequisite: "HTML"
-    },
-    {
-        name: "Data Science",
-        tutor: "Lina Ahmed",
-        prerequisite: "Python"
-    },
-    {
-        name: "Game Development",
-        tutor: "Ali Hassan",
-        prerequisite: "Introduction to Programming"
-    }
-];
-
-// Function to get unique tutor names
-function getUniqueTutors() {
-    return [...new Set(courses.map(course => course.tutor))];
-}
-
-// Function to get unique prerequisite names
-function getUniquePrerequisites() {
-    return [...new Set(courses.filter(course => course.prerequisite).map(course => course.prerequisite))];
-}
-
-// Function to filter courses by tutor or prerequisite
-function filterCourses(filterValue, filterType) {
-    return courses.filter(course => course[filterType] === filterValue);
-}
-
-// Example usage:
-var selectedTutor = "Sami Ahmed";
-var selectedPrerequisite = "Python";
-
-var filteredCoursesByTutor = filterCourses(selectedTutor, "tutor");
-var filteredCoursesByPrerequisite = filterCourses(selectedPrerequisite, "prerequisite");
-
-// Assuming you have a form with an ID 'courseForm', you can dynamically fill in the form
-function populateForm(courses) {
-    var form = document.getElementById('courseForm');
-
-    // Clear existing options
-    form.innerHTML = '';
-
-    courses.forEach(course => {
-        var option = document.createElement('option');
-        option.value = course.name;
-        option.text = course.name;
-        form.add(option);
-    });
-}
-
-// Example usage:
-populateForm(filteredCoursesByTutor);
-// or
-populateForm(filteredCoursesByPrerequisite);
-
-
-//5,6,7
+//2,3
 
 document.addEventListener("DOMContentLoaded", function () {
-    var form = document.getElementById("courseForm");
-    form.addEventListener("submit", function (event) {
-        event.preventDefault(); // Prevent the default form submission behavior
-
-        // Validate form inputs
-        var selectedChild = form.elements["child"].value;
-        var selectedCourse = form.elements["course"].value;
-
-        if (!selectedChild || !selectedCourse) {
-            alert("Please select a child and a course.");
-            return;
+    var courses = [
+        {
+            name: "Python",
+            tutor: "Mona Khaled",
+            prerequisite: "SQL",
+            className: "course-item-Python"
+        },
+        {
+            name: "Java",
+            tutor: "Amal Abdullah",
+            prerequisite: null,
+            className: "course-item-Java"
+        },
+        {
+            name: "HTML",
+            tutor: "Fahad Omar",
+            prerequisite: null,
+            className: "course-item-HTML"
+        },
+        {
+            name: "Scratch",
+            tutor: "Najwa Hamad",
+            prerequisite: null,
+            className: "course-item-Scratch"
+        },
+        {
+            name: "Robotics",
+            tutor: "Sami Ahmed",
+            prerequisite: "Python",
+            className: "course-item-Robotics"
+        },
+        {
+            name: "Machine Learning",
+            tutor: "Hind Saleh",
+            prerequisite: "Python",
+            className: "course-item-MachineLearning"
+        },
+        {
+            name: "SQL",
+            tutor: "Najwa Hamad",
+            prerequisite: "Scratch",
+            className: "course-item-SQL"
+        },
+        {
+            name: "CSS",
+            tutor: "Fahad Omar",
+            prerequisite: "HTML",
+            className: "course-item-CSS"
+        },
+        {
+            name: "JavaScript",
+            tutor: "Amal Abdullah",
+            prerequisite: "Java",
+            className: "course-item-javascript"
         }
+    ];
 
-        // Assuming you have an element with the ID "output" to display the information
-        var outputElement = document.getElementById("output");
-
-        // Clear previous information
-        outputElement.innerHTML = "";
-
-        // Display new information
-        var childInfo = localStorage.getItem("childInfo");
-        if (childInfo) {
-            childInfo = JSON.parse(childInfo);
-            var childName = childInfo.name;
-            var selectedTutor = getTutorForCourse(selectedCourse);
-            
-            outputElement.innerHTML = `<p>Child Name: ${childName}</p>`;
-            outputElement.innerHTML += `<p>Selected Course: ${selectedCourse}</p>`;
-            outputElement.innerHTML += `<p>Tutor: ${selectedTutor}</p>`;
+    // Populate the prerequisite filter dropdown
+    var prerequisiteFilterDropdown = document.getElementById("prerequisite-filter");
+    var prerequisiteOptions = new Set();
+    courses.forEach(function (course) {
+        if (course.prerequisite) {
+            prerequisiteOptions.add(course.prerequisite);
         }
-
-        // Clear the form
-        form.reset();
     });
 
-    // Function to get tutor for a specific course
-    function getTutorForCourse(courseName) {
-        var course = courses.find(course => course.name === courseName);
-        return course ? course.tutor : "Tutor not found";
-    }
+    // Add options to the dropdown
+    var allOption = document.createElement("option");
+    allOption.value = "all";
+    allOption.textContent = "All Prerequisites";
+    prerequisiteFilterDropdown.appendChild(allOption);
+
+    prerequisiteOptions.forEach(function (prerequisite) {
+        var option = document.createElement("option");
+        option.value = prerequisite;
+        option.textContent = prerequisite;
+        prerequisiteFilterDropdown.appendChild(option);
+    });
+
+    // Initial filtering
+    filterCourses();
 });
+
+function filterCourses() {
+    var selectedPrerequisite = document.getElementById("prerequisite-filter").value;
+    var courseItems = document.querySelectorAll(".course-list [class^='course-item-']");
+
+    courseItems.forEach(function (courseItem) {
+        var coursePrerequisite = courseItem.dataset.prerequisite;
+        var showCourse = !selectedPrerequisite || (selectedPrerequisite === "all") || (selectedPrerequisite === coursePrerequisite);
+        courseItem.style.display = showCourse ? "block" : "none";
+    });
+}
+
+// Attach the filterCourses function to the change event of the prerequisite filter dropdown
+document.getElementById("prerequisite-filter").addEventListener("change", filterCourses); 
+
---
+++ adm.js
@@ -0,0 +1,82 @@
+function validateForm() {
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var gender = document.querySelector('input[name="gender"]:checked');
+    var age = document.getElementById("age").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var photo = document.getElementById("photo").value;
+
+    if (fname === "" || lname === "" || !gender || age === "" || email === "" || phone === "") {
+        alert("Please fill in all fields.");
+        return false;
+    }
+
+    if (!/^[a-zA-Z]+$/.test(fname) || !/^[a-zA-Z]+$/.test(lname)) {
+        alert("Name fields should start with a letter.");
+        return false;
+    }
+
+    if (phone.length !== 10 || !/^\d+$/.test(phone)) {
+        alert("Please enter a valid 10-digit phone number.");
+        return false;
+    }
+
+    var birthDate = new Date(age);
+    var currentDate = new Date();
+    var cutoffDate = new Date("2017-01-01");
+
+    if (birthDate > currentDate || birthDate > cutoffDate) {
+        alert("Date of Birth cannot be after 2017.");
+        return false;
+    }
+
+    if (fname !== "" && lname !== "" && gender && age !== "" && email !== "" && phone !== "") {
+        var childName = fname + " " + lname;
+        localStorage.setItem("childName", childName);
+
+        console.log("Child's name stored in local storage:", childName);
+
+        var childInfo = {
+            photo: document.getElementById("photo").files[0],
+            name: fname,
+            gender: gender.value,
+            age: age,
+            email: email,
+            phone: phone
+        };
+        localStorage.setItem("childInfo", JSON.stringify(childInfo));
+        console.log("before console");
+
+        console.log("Child information stored in local storage:", childInfo);
+        console.log("after console");
+        displayChildInformation(childInfo);
+    }
+
+    return true;
+}
+
+function displayChildInformation(childInfo) {
+  
+    var newWindow = window.open('', '_blank');
+
+    newWindow.document.write('<html><head></head><body style="margin: 20px; padding: 20px; box-sizing: border-box;">');
+
+    
+    newWindow.document.write('<div style="border: 1px solid black; padding: 10px; box-sizing: border-box;">');
+
+    var imageUrl = URL.createObjectURL(childInfo.photo);
+
+    
+    newWindow.document.write('<img src="' + imageUrl + '" alt="Child\'s Photo" style="width: 100px; height: 100px; margin-bottom: 10px; box-sizing: border-box;">');
+
+  
+    newWindow.document.write('<p>Name: ' + childInfo.name + '</p>');
+    newWindow.document.write('<p>Gender: ' + childInfo.gender + '</p>');
+    newWindow.document.write('<p>DOB: ' + childInfo.age + '</p>');
+    newWindow.document.write('<p>Email: ' + childInfo.email + '</p>');
+    newWindow.document.write('<p>Phone: ' + childInfo.phone + '</p>');
+
+    newWindow.document.write('</div></body></html>');
+    newWindow.document.close();
+}
