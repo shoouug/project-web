b68f70f08f56226a0bfab22ac3547754052cf2a3
Saraytm <saraytm44@gmail.com>
2023-11-23 06:33:57 +0300

    js

---
+++ evaluation.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var feedbackForm = document.getElementById('feedback-form');
+    var courseSelect = document.getElementById('course');
+    var ratingStars = document.querySelectorAll('.star');
+
+    function isFormValid() {
+        var selectedCourse = courseSelect.value;
+        var isRatingSelected = Array.from(ratingStars).some(star => star.classList.contains('checked'));
+
+        return selectedCourse && isRatingSelected;
+    }
+
+    function displayAlert(course, rating) {
+        alert("Thank you for your feedback!\n Your rating for course " + course + " is " + rating + " out of 5 ");
+    }
+
+    feedbackForm.addEventListener('submit', function (event) {
+        if (isFormValid()) {
+            var selectedCourse = courseSelect.value;
+            var userRating = document.querySelectorAll('.star.checked').length;
+            displayAlert(selectedCourse, userRating);
+        } else {
+            alert("Please provide a rating for the selected course before submitting");
+        }
+    });
+});
