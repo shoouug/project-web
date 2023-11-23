926b88e31387ce116401f616c7b95a550134601d
AbeerAlnafaei <144719854+AbeerAlnafaei@users.noreply.github.com>
2023-11-23 05:28:18 +0300

    theme

--- theme-switch.js
+++ theme-switch.js
@@ -1,22 +1,5 @@
 // JavaScript source code
-document.addEventListener("DOMContentLoaded", function () {
-    const themeToggle = document.getElementById("theme-toggle");
-
-    // Check the user's theme preference from local storage
-    const currentTheme = localStorage.getItem("theme");
-    if (currentTheme) {
-        document.body.classList.add(currentTheme);
-        themeToggle.checked = currentTheme === "theme-dark";
-    }
-
-    // Toggle the theme when the switch is changed
-    themeToggle.addEventListener("change", function () {
-        if (themeToggle.checked) {
-            document.body.classList.add("theme-dark");
-            localStorage.setItem("theme", "theme-dark");
-        } else {
-            document.body.classList.remove("theme-dark");
-            localStorage.setItem("theme", "theme-light");
-        }
-    });
-});
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    document.body.classList.toggle("dark-mode") 
+}
