02c6461dd66009525bbd1ffef1d11389947a609a
AbeerAlnafaei <144719854+AbeerAlnafaei@users.noreply.github.com>
2023-11-23 05:42:51 +0300

    theme

--- theme-switch.js
+++ theme-switch.js
@@ -1,5 +1,28 @@
-// JavaScript source code
-var icon = document.getElementById("icon");
-icon.onclick = function () {
-    document.body.classList.toggle("dark-mode") 
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const themeToggle = document.getElementById("theme-toggle");
+    const modeText = document.getElementById("mode-text");
+    const body = document.body;
+
+    // Check if dark mode is stored in localStorage
+    const isDarkMode = localStorage.getItem("darkMode") === "true";
+
+    // Apply dark mode if previously set
+    if (isDarkMode) {
+        body.classList.add("dark-mode");
+        modeText.textContent = "Dark Mode";
+    }
+
+    themeToggle.addEventListener("click", toggleDarkMode);
+
+    function toggleDarkMode() {
+        // Toggle dark mode class on body
+        body.classList.toggle("dark-mode");
+
+        // Toggle mode text
+        const isDarkModeNow = body.classList.contains("dark-mode");
+        modeText.textContent = isDarkModeNow ? "Light Mode" : "Dark Mode";
+
+        // Store the current mode in localStorage
+        localStorage.setItem("darkMode", isDarkModeNow);
+    }
+});
