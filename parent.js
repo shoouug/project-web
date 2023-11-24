925c90e10094fbc5d0ca3153edef9207dbcb1147
Saraytm <saraytm44@gmail.com>
2023-11-24 05:51:59 +0300

    lo

--- parent.js
+++ parent.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Check if parentChildren key exists in local storage
     var storedChildren = JSON.parse(localStorage.getItem("parentChildren"));
 
     if (!storedChildren) {
-        // If not, initialize with default names
         var childrenNames = ["Ali Ahmad", "Fahad Ahmad"];
         localStorage.setItem("parentChildren", JSON.stringify(childrenNames));
         displayChildren(childrenNames);
     } else {
-        // If exists, display the stored children's names
         displayChildren(storedChildren);
     }
 });
@@ -16,10 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
 function displayChildren(childrenNames) {
     var wrapper = document.querySelector(".wrapper");
 
-    // Clear existing content in the wrapper
     wrapper.innerHTML = "";
 
-    // Create and append child boxes
     childrenNames.forEach(function (childName) {
         var childElement = document.createElement("div");
         childElement.classList.add("child-box");
