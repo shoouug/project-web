4837be1c0df30e011219bac6d0b4fcdc0cf23620
Saraytm <saraytm44@gmail.com>
2023-11-25 02:35:21 +0300

    bh

--- parent.js
+++ parent.js
@@ -2,16 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     const storedChildren = localStorage.getItem('childName');
   
     if (storedChildren) {
-      const childrenNames = JSON.parse(storedChildren);
+      var childrenNames = JSON.parse(storedChildren);
       displayChildren(childrenNames);
     }
   
     function displayChildren(names) {
-      const wrapper = document.querySelector('.wrapper');
+      var wrapper = document.querySelector('.wrapper');
       wrapper.innerHTML = ''; 
   
       names.forEach(name => {
-        const childBox = document.createElement('div');
+        var childBox = document.createElement('div');
         childBox.classList.add('child-box');
         childBox.textContent = name;
         wrapper.appendChild(childBox);
