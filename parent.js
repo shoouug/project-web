338df3e73eddfda9cfc92a4b664d961f601b6477
Saraytm <saraytm44@gmail.com>
2023-11-25 03:32:30 +0300

    update

--- parent.js
+++ parent.js
@@ -4,14 +4,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var childName = localStorage.getItem("childName");
 
-    // Check if childName exists
-    if (childName && !kidsNames.includes(childName)) {
-        kidsNames.push(childName);
-        localStorage.setItem("kidsNames", JSON.stringify(kidsNames));
-        displayChildren(kidsNames);
-    }
+    // Check if kidsNames array is empty
+    if (kidsNames.length === 0) {
+        // If it is empty, save 2 children's names to local storage
+        const defaultChildrenNames = ['Ahmad Ali', 'Fahad Ali'];
+        localStorage.setItem("kidsNames", JSON.stringify(defaultChildrenNames));
 
-   
+        // Display the default children's names in the dashboard
+        displayChildren(defaultChildrenNames);
+    } else {
+        // If not empty, check if childName exists
+        if (childName) {
+            // Add the new child name to the array if it doesn't exist
+            if (!kidsNames.includes(childName)) {
+                kidsNames.push(childName);
+
+                // Update local storage with the modified array
+                localStorage.setItem("kidsNames", JSON.stringify(kidsNames));
+            }
+        }
+
+        // Display the children's names from local storage (excluding the default 2 names) in the dashboard
+        displayChildren(kidsNames.filter(name => !['Ahmad Ali', 'Fahad Ali'].includes(name)));
+    }
 
     function displayChildren(names) {
         // Display children's names in the dashboard
