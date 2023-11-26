a7868fb75cfc8191b209ae6a8fb7833ed50fd6c7
Saraytm <saraytm44@gmail.com>
2023-11-26 16:29:02 +0300

    u

--- parent.js
+++ parent.js
@@ -1,28 +1,23 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    var childName = JSON.parse(localStorage.getItem("childName")) || [];
-
-    if (childName.length === 0) {
-        // Save 2 default children's names if local storage is empty
+    var kidsNames = JSON.parse(localStorage.getItem("kidsNames")) || [];
+    var childName = localStorage.getItem("childName");
+    if (kidsNames.length === 0) {
         var defaultChildrenNames = ['Ahmad Ali', 'Fahad Ali'];
-        localStorage.setItem("childName", JSON.stringify(defaultChildrenNames));
+        localStorage.setItem("kidsNames", JSON.stringify(defaultChildrenNames));
         displayChildren(defaultChildrenNames);
     } else {
-        // Remove default names if present
-        var defaultNamesExist = childName.some(name => ['Ahmad Ali', 'Fahad Ali'].includes(name));
-        if (defaultNamesExist) {
-            childName = childName.filter(name => !['Ahmad Ali', 'Fahad Ali'].includes(name));
-            localStorage.setItem("childName", JSON.stringify(childName));
+        if (childName) {
+            if (!kidsNames.includes(childName)) {
+                kidsNames.push(childName);
+                localStorage.setItem("kidsNames", JSON.stringify(kidsNames));
+            }
+            
         }
-
-        displayChildren(childName);
+        displayChildren(kidsNames);
     }
 
     function displayChildren(names) {
         var wrapper = document.querySelector('.wrapper');
-        wrapper.innerHTML = ""; // Clear existing children
-
         names.forEach(name => {
             var childBox = document.createElement('div');
             childBox.classList.add('child-box');
@@ -31,3 +26,4 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
