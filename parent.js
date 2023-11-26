707448a6c8b5fc59eb0c42f74d4ce8cce839effb
Saraytm <saraytm44@gmail.com>
2023-11-26 15:55:10 +0300

    up

--- parent.js
+++ parent.js
@@ -1,23 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     var kidsNames = JSON.parse(localStorage.getItem("kidsNames")) || [];
     var childName = localStorage.getItem("childName");
+
     if (kidsNames.length === 0) {
         var defaultChildrenNames = ['Ahmad Ali', 'Fahad Ali'];
-        localStorage.setItem("kidsNames", JSON.stringify(defaultChildrenNames));
+        sessionStorage.setItem("kidsNames", JSON.stringify(defaultChildrenNames));
         displayChildren(defaultChildrenNames);
     } else {
-        if (childName) {
-            if (!kidsNames.includes(childName)) {
-                kidsNames.push(childName);
-                localStorage.setItem("kidsNames", JSON.stringify(kidsNames));
-            }
-            
+        if (childName && !kidsNames.includes(childName)) {
+            kidsNames = kidsNames.filter(name => !['Ahmad Ali', 'Fahad Ali'].includes(name));
+
+            kidsNames.push(childName);
+            localStorage.setItem("kidsNames", JSON.stringify(kidsNames));
         }
         displayChildren(kidsNames);
     }
 
     function displayChildren(names) {
         var wrapper = document.querySelector('.wrapper');
+        wrapper.innerHTML = ""; 
+
         names.forEach(name => {
             var childBox = document.createElement('div');
             childBox.classList.add('child-box');
