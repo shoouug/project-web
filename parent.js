33486d71281fb26c79c67a3ed60c0eb6d03981c7
Saraytm <saraytm44@gmail.com>
2023-11-26 19:41:44 +0300

    nn

--- parent.js
+++ parent.js
@@ -1,22 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     var childName = localStorage.getItem("childName");
+    var kidsNames = JSON.parse(localStorage.getItem("kidsNames")) || [];
     
-    if (!childName) {
+    if (!childName && kidsNames.length === 0) {
         var defaultChildrenNames = ['Ahmad Ali', 'Fahad Ali'];
         localStorage.setItem("kidsNames", JSON.stringify(defaultChildrenNames));
         displayChildren(defaultChildrenNames);
     } else {
-        var kidsNames = JSON.parse(localStorage.getItem("kidsNames")) || [];
-        
-        // Check if there are no kidsNames saved
-        if (kidsNames.length === 0) {
-            // If empty, display default names
-            var defaultChildrenNames = ['Ahmad Ali', 'Fahad Ali'];
-            displayChildren(defaultChildrenNames);
-        } else {
-            // Otherwise, display the saved names
-            displayChildren(kidsNames);
-        }
+        displayChildren(kidsNames);
     }
 
     function displayChildren(names) {
