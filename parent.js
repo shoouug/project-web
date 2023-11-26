df299c2a203fd38cc5c39d63862438f7eb55945b
Saraytm <saraytm44@gmail.com>
2023-11-26 16:27:34 +0300

    uo

--- parent.js
+++ parent.js
@@ -1,23 +1,28 @@
+
+
 document.addEventListener('DOMContentLoaded', function () {
-    var kidsNames = JSON.parse(localStorage.getItem("kidsNames")) || [];
-    var childName = localStorage.getItem("childName");
-    if (kidsNames.length === 0) {
+    var childName = JSON.parse(localStorage.getItem("childName")) || [];
+
+    if (childName.length === 0) {
+        // Save 2 default children's names if local storage is empty
         var defaultChildrenNames = ['Ahmad Ali', 'Fahad Ali'];
-        localStorage.setItem("kidsNames", JSON.stringify(defaultChildrenNames));
+        localStorage.setItem("childName", JSON.stringify(defaultChildrenNames));
         displayChildren(defaultChildrenNames);
     } else {
-        if (childName) {
-            if (!kidsNames.includes(childName)) {
-                kidsNames.push(childName);
-                localStorage.setItem("kidsNames", JSON.stringify(kidsNames));
-            }
-            
+        // Remove default names if present
+        var defaultNamesExist = childName.some(name => ['Ahmad Ali', 'Fahad Ali'].includes(name));
+        if (defaultNamesExist) {
+            childName = childName.filter(name => !['Ahmad Ali', 'Fahad Ali'].includes(name));
+            localStorage.setItem("childName", JSON.stringify(childName));
         }
-        displayChildren(kidsNames);
+
+        displayChildren(childName);
     }
 
     function displayChildren(names) {
         var wrapper = document.querySelector('.wrapper');
+        wrapper.innerHTML = ""; // Clear existing children
+
         names.forEach(name => {
             var childBox = document.createElement('div');
             childBox.classList.add('child-box');
