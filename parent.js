93a047daa0a7719e7ac24d557406e23afc48a730
Saraytm <saraytm44@gmail.com>
2023-11-26 17:52:06 +0300

    jj

--- parent.js
+++ parent.js
@@ -2,32 +2,25 @@ document.addEventListener('DOMContentLoaded', function () {
     var allNames = JSON.parse(localStorage.getItem("allNames")) || [];
     var childName = localStorage.getItem("childName");
 
-    if (allNames.length === 0) {
+    if (!childName) {
         var defaultChildrenNames = ['Ahmad Ali', 'Fahad Ali'];
-        allNames = allNames.concat(defaultChildrenNames);
-        localStorage.setItem("allNames", JSON.stringify(allNames));
-    }
-
-    if (childName && !allNames.includes(childName)) {
+        if (!allNames.length) {
+            allNames = allNames.concat(defaultChildrenNames);
+            localStorage.setItem("allNames", JSON.stringify(allNames));
+        }
+    } else if (!allNames.includes(childName)) {
         allNames.push(childName);
         localStorage.setItem("allNames", JSON.stringify(allNames));
     }
 
-    displayAllNames(allNames);
+    displayAllNames(allNames.slice(2));
 
     function displayAllNames(names) {
         var wrapper = document.querySelector('.wrapper');
         // Clear previous content
         wrapper.innerHTML = '';
 
-        names.slice(0, 2).forEach(name => {
-            var childBox = document.createElement('div');
-            childBox.classList.add('child-box');
-            childBox.textContent = name;
-            wrapper.appendChild(childBox);
-        });
-
-        names.slice(2).forEach(name => {
+        names.forEach(name => {
             var childBox = document.createElement('div');
             childBox.classList.add('child-box');
             childBox.textContent = name;
