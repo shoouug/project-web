ae4269a57783f618d5087fa1aff3e27cbe2d9b5a
Saraytm <saraytm44@gmail.com>
2023-11-26 17:24:52 +0300

    mm

--- parent.js
+++ parent.js
@@ -11,8 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function displayChildName(names) {
         var wrapper = document.querySelector('.wrapper');
-        // Clear previous content
-        wrapper.innerHTML = '';
+
 
         names.forEach(name => {
             var childBox = document.createElement('div');
