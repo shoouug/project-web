50758cf62b6a43f8313bbe8fd70edb005a9a6fbc
Saraytm <saraytm44@gmail.com>
2023-11-26 19:05:49 +0300

    mm

--- parent.js
+++ parent.js
@@ -2,23 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
     var allNames = JSON.parse(localStorage.getItem("allNames")) || [];
     var childName = localStorage.getItem("childName");
 
-    var defaultChildrenNames = ['Ahmad Ali', 'Fahad Ali'];
-
-    if (!childName) {
-        if (allNames.length === 0) {
-            allNames = allNames.concat(defaultChildrenNames);
-            localStorage.setItem("allNames", JSON.stringify(allNames));
-        }
-    } else {
-        // Add the new name
-        allNames.push(childName);
-
-        // Remove default names after adding a new name
-        allNames = allNames.slice(defaultChildrenNames.length);
+    if (allNames.length === 0) {
+        var defaultChildrenNames = ['Ahmad Ali', 'Fahad Ali'];
+        allNames = allNames.concat(defaultChildrenNames);
         localStorage.setItem("allNames", JSON.stringify(allNames));
+        displayAllNames("allNames")
     }
 
-    displayAllNames(allNames);
+    if (childName && !allNames.includes(childName)) {
+        allNames.push(childName);
+        localStorage.setItem("allNames", JSON.stringify(allNames));
+        displayAllNames(allNames.slice(2));
+    }
+
+
 
     function displayAllNames(names) {
         var wrapper = document.querySelector('.wrapper');
